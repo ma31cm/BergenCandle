0ad9279b58c24e4f0c5abb86663931a428ad278e
Catriona MacKenzie <catriona.mackenzie@bouvet.no>
2024-08-02 15:11:48 +0200

    Copy nextjs output to docs

---
+++ docs/_next/static/chunks/app/page-17236912c9054652.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,301],{3822:function(n,e,t){Promise.resolve().then(t.t.bind(t,9799,23)),Promise.resolve().then(t.t.bind(t,6120,23))},9799:function(){},6120:function(){}},function(n){n.O(0,[694,971,23,744],function(){return n(n.s=3822)}),_N_E=n.O()}]);
